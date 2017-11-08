471a394c967a617345f5a5508afd4fcfb9143912
TheCurryMan <avinashkalpitjain@gmail.com>
2017-11-07 20:37:38 -0800

    Added Android Activity Source Code

---
+++ ActivityApp/app/src/main/java/com/thecodex/mdcweek5/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.thecodex.mdcweek5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void onclick(View v){
+        Intent myIntent = new Intent(Main2Activity.this, MainActivity.class);
+        Main2Activity.this.startActivity(myIntent);
+    }
+}
